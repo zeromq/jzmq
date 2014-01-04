4ecb7a0996da4336d8b1c2e7bcd0133838a4b7e8
James Gatannah <james.gatannah@gmail.com>
2014-01-04 10:02:45 -0600

    My machine seemed to recognize memcpy w/out a header declaring it. Travis didn't. Try including the one that cplusplus.com lists as the official source

--- src/main/c++/ZMQ.cpp
+++ src/main/c++/ZMQ.cpp
@@ -17,9 +17,9 @@
     along with this program.  If not, see <http://www.gnu.org/licenses/>.
 */
 
+#include <cstring>
+
 #include <zmq.h>
-// Needed for the curve pieces. Seems like a strong indicator that their
-// equivalents belong in util.cpp here.
 #include <zmq_utils.h>
 
 #include "jzmq.hpp"
