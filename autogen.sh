b0d8a9bd8cea973ca81d960512eae95cec8b6c73
jack9 <jack9@github.jack9.org>
2016-02-02 13:32:29 -0800

    databricks libtool is weird

--- autogen.sh
+++ autogen.sh
@@ -24,7 +24,7 @@ if  [ $? -ne 0 ]; then
     exit 1
 fi
 
-command -v libtool >/dev/null 2>&1
+command -v libtoolize >/dev/null 2>&1
 if  [ $? -ne 0 ]; then
     echo "autogen.sh: error: could not find libtool.  libtool is required to run autogen.sh." 1>&2
     exit 1
