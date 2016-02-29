8c8d55a9de3f68435467d5fc1d11f57edf2cf365
lukepalmer <luke@lukepalmer.net>
2016-02-28 23:27:35 -0500

    Load multiple native libraries

--- jzmq-jni/src/test/java/org/zeromq/EmbeddedLibraryToolsTest.java
+++ jzmq-jni/src/test/java/org/zeromq/EmbeddedLibraryToolsTest.java
@@ -1,14 +1,24 @@
 package org.zeromq;
 
+import org.easymock.Capture;
+import org.easymock.CaptureType;
 import org.junit.After;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.powermock.core.classloader.annotations.PrepareForTest;
+import org.powermock.modules.junit4.PowerMockRunner;
 
+import static org.easymock.EasyMock.capture;
+import static org.easymock.EasyMock.newCapture;
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+import static org.powermock.api.easymock.PowerMock.*;
 
 /**
  * Exercises basic manipulations in the EmbeddedLibraryTools for JNI loading.
  * Does not give full coverage yet as this has to be per-platform.
  */
+@RunWith(PowerMockRunner.class)
 public class EmbeddedLibraryToolsTest {
     private static final String origOsName = System.getProperty("os.name");
 
@@ -45,6 +55,42 @@ public class EmbeddedLibraryToolsTest {
         setOsProperties("Digital Unix", "alpha");
         assertPlatformIdentifierEquals("alpha/Digital_Unix");
     }
+
+    @Test
+    @PrepareForTest(EmbeddedLibraryTools.class)
+    public void multiLibLoad() throws Exception {
+        mockStaticPartial(System.class, "load");
+        Capture<String> loadCapture = newCapture(CaptureType.ALL);
+        System.load(capture(loadCapture));
+        expectLastCall().times(2);
+        replayAll();
+        setOsProperties("Linux", "testarch");
+        assertTrue(EmbeddedLibraryTools.LOADED_EMBEDDED_LIBRARY);
+        verifyAll();
+        assertTrue(loadCapture.getValues().get(0).contains("libzmq"));
+        assertTrue(loadCapture.getValues().get(1).contains("libjzmq"));
+    }
+
+    @Test
+    @PrepareForTest(EmbeddedLibraryTools.class)
+    public void libsAsProp() throws Exception {
+        try {
+            System.setProperty("jzmq.libs", "foo,libjzmq");
+            mockStaticPartial(System.class, "load");
+            Capture<String> loadCapture = newCapture(CaptureType.ALL);
+            System.load(capture(loadCapture));
+            expectLastCall().times(2);
+            replayAll();
+            setOsProperties("Linux", "testarch");
+            assertTrue(EmbeddedLibraryTools.LOADED_EMBEDDED_LIBRARY);
+            verifyAll();
+            assertTrue(loadCapture.getValues().get(0).contains("foo"));
+            assertTrue(loadCapture.getValues().get(1).contains("libjzmq"));
+        } finally {
+            System.clearProperty("jzmq.libs");
+        }
+    }
+
 
     @After
     public void resetOsProperties() {
