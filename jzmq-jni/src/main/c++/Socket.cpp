5d0d65b2427b79edc2c55e54f20a6e15a40cb223
ljj7kr <ljj7kr@gmail.com>
2017-07-04 19:44:04 +0900

    Update Socket.cpp

--- jzmq-jni/src/main/c++/Socket.cpp
+++ jzmq-jni/src/main/c++/Socket.cpp
@@ -148,7 +148,9 @@ JNIEXPORT void JNICALL Java_org_zeromq_ZMQ_00024Socket_destroy (JNIEnv *env, job
 JNIEXPORT jlong JNICALL
 Java_org_zeromq_ZMQ_00024Socket_getLongSockopt (JNIEnv *env, jobject obj, jint option)
 {
-    switch (option) {
+
+	
+	switch (option) {
 #if ZMQ_VERSION >= ZMQ_MAKE_VERSION(3,0,0)
     case ZMQ_BACKLOG:
     case ZMQ_MAXMSGSIZE:
@@ -391,12 +393,12 @@ JNIEXPORT void JNICALL Java_org_zeromq_ZMQ_00024Socket_setLongSockopt (JNIEnv *e
                 || (option == ZMQ_REQ_RELAXED)
                 || (option == ZMQ_REQ_CORRELATE)
                 || (option == ZMQ_PROBE_ROUTER)
-                || (option == ZMQ_CURVE_SERVER)
-		|| (option == ZMQ_ROUTER_HANDOVER)
+                || (option == ZMQ_CURVE_SERVER)	
 #endif
 #if ZMQ_VERSION >= ZMQ_MAKE_VERSION(4,1,0)
                 || (option == ZMQ_GSSAPI_SERVER)
                 || (option == ZMQ_GSSAPI_PLAINTEXT)
+		|| (option == ZMQ_ROUTER_HANDOVER)
 #endif
 #if ZMQ_VERSION >= ZMQ_MAKE_VERSION(2,1,0)
             ) {
